824314e97cf204eb8012e5ca4362792b6be8edf8
Jorge Silva <jorgebsilva69@gmail.com>
2026-03-09 07:26:22 -0700

    Exercicios 10

---
+++ Exercicios_Loop/Exercicio10.ps1
@@ -0,0 +1,11 @@
+﻿$num = Read-Host "Introduza um número"
+$contadorDivisores = 0
+
+for ($i = 1; $i -le $num; $i++) {
+    
+    if ($num % $i -eq 0) {
+        $contadorDivisores = $contadorDivisores + 1
+    }
+}
+
+Write-Host "O número $num possui $contadorDivisores divisores."

bca993b86fb21fcba167f6bd0458ad12672693ba
Jorge Silva <jorgebsilva69@gmail.com>
2026-03-09 06:36:51 -0700

    Exercicios 6

---
+++ Exercicios_Loop/Exercicio6.ps1
@@ -0,0 +1,20 @@
+﻿$primosEncontrados = 0
+$num = 2
+
+while ($primosEncontrados -lt 10) {
+    $ePrimo = $true
+    
+    for ($i = 2; $i -lt $num; $i++) {
+        if ($num % $i -eq 0) {
+            $ePrimo = $false
+            break
+        }
+    }
+
+    if ($ePrimo -eq $true) {
+        Write-Host "Primo: $num"
+        $primosEncontrados++
+    }
+    
+    $num++
+}
